860d07e247395955cc6a5f8ba707f67d7fb0778a
Kajalpatil29 <100278179+Kajalpatil29@users.noreply.github.com>
2022-02-23 22:31:32 +0530

    Add files via upload

---
+++ CombinationExample.java
@@ -0,0 +1,35 @@
+package PermutationAndCombination;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationExample
+{
+	
+		   static int fact(int number) {  
+		      int f = 1;  
+		      int j = 1;  
+		      while(j <= number) {  
+		         f = f * j;  
+		         j++;  
+		      }  
+		      return f;  
+		   }  
+		   public static void main(String args[]) {  
+		         
+		      List<Integer> numbers = new ArrayList<Integer>();  
+		        
+		      numbers.add(9);  
+		      numbers.add(12);  
+		    
+		        
+		      int n = numbers.size();  
+		      int r = 2;  
+		      int result;  
+		        
+		      result = fact(n) / (fact(r) * fact(n-r));  
+		      System.out.println("The combination value for the numbers list is: " + result);  
+		     
+		}  
+
+}
